26c960dec77ce8584a5c38eb7e4f7f9129b8d495
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-02-02 19:31:40 +0200

    Add function to get data from api

---
+++ src/js/data.js
@@ -0,0 +1,39 @@
+import { kelvinToCelsius, kelvinToFahrenheit } from "./conversion";
+
+function getName(name) {
+    return name.trim()
+        .replace(/,\s/g, ",")
+        .replace(/\s,/g, ",")
+        .replace(/\s/, "+")
+}
+
+function filterData(data) {
+    return {
+        name: data.name,
+        icon: data.weather[0].icon,
+        description: data.weather[0].description,
+        temp: {
+            celsius: kelvinToCelsius(data.main.temp) + "°C",
+            fahrenheit: kelvinToFahrenheit(data.main.temp) + "°F"
+        },
+        feelsLike: {
+            celsius: kelvinToCelsius(data.main.feels_like) + "°C",
+            fahrenheit: kelvinToFahrenheit(data.main.feels_like) + "°F"
+        },
+        humidity: data.main.humidity + "%",
+        windSpeed: {
+            kmph: Math.round(data.wind.speed * 3.6) + " km/h",
+            mph: Math.round(data.wind.speed * 2.237) + " mph"
+        }
+    };
+}
+
+async function getWeatherData(location) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${getName(location)}&appid=f10b79e03b5de9d11241b8d0e6e46cd3`;
+    const response = await fetch(url, { mode: "cors" });
+    const weatherData = await response.json();
+    const data = filterData(weatherData);
+    return data;
+}
+
+export default getWeatherData;
