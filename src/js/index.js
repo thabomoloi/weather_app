58e4d2af70533c85cf9bccf55e0837cc21e1409f
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-02-02 21:11:14 +0200

    Add search event listener

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,18 @@ import Main from "./dom/Main";
 import getWeatherData from "./data";
 
 async function createView() {
-    const data = await getWeatherData("Cape Town");
-    new Main(data);
+    var data = await getWeatherData("Cape Town");
+    const mainView = new Main(data);
+
+    const btnSearch = document.querySelector("button.btn-search");
+    const searchText = document.querySelector("input.city-search");
+
+    if (btnSearch && searchText) {
+        btnSearch.addEventListener("click", async () => {
+            data = await getWeatherData(searchText.value);
+            mainView.displayNewData(data);
+            searchText.value = "";
+        });
+    }
 }
 createView();
