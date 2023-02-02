a04b2efc6540f432e099a5148be9d8742dea8602
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-02-02 14:48:11 +0200

    Add header view class

---
+++ src/js/dom/Header.js
@@ -0,0 +1,22 @@
+class Header {
+    constructor() {
+        this.header = document.createElement("header");
+
+        this.searchDiv = document.createElement("div");
+
+        this.searchInput = document.createElement("input");
+        this.searchInput.classList.add("city-search");
+        this.searchInput.placeholder = "Enter A City";
+
+        this.searchButton = document.createElement("button");
+        this.searchButton.classList.add("btn-search", "btn");
+        this.searchButton.innerText = "SEARCH";
+
+        this.searchDiv.append(this.searchInput, this.searchButton);
+        this.header.appendChild(this.searchDiv);
+
+        document.querySelector("body")?.appendChild(this.header);
+    }
+}
+
+export default Header;
