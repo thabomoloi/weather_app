0cd8f034e501897f311e3867032b6345ba1764e9
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-02-03 18:46:02 +0200

    Use .then method instead of await

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,29 @@
+import "../css/style.css";
 
 import Main from "./dom/Main";
 import getWeatherData from "./data";
 
-async function createView() {
-    var data = await getWeatherData("Cape Town");
-    const mainView = new Main(data);
+
+
+async function createView(name) {
+    var data = await getWeatherData(name);
+    var mainView = new Main(data);
 
     const btnSearch = document.querySelector("button.btn-search");
     const searchText = document.querySelector("input.city-search");
 
     if (btnSearch && searchText) {
-        btnSearch.addEventListener("click", async () => {
-            data = await getWeatherData(searchText.value);
-            mainView.displayNewData(data);
-            searchText.value = "";
+
+        btnSearch.addEventListener("click", () => {
+            createView(searchText.value).then(function () {
+                searchText.value = "";
+            }).catch(function (error) {
+                createView("Cape Town");
+            });
         });
     }
 }
-createView();
+
+
+createView("Cape Town");
+
