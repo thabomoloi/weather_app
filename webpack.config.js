350b7f222d18d64843aca19ce85d3c17d895bc5d
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-02-03 18:41:54 +0200

    Add image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,14 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
 };
